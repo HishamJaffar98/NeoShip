3c2bccf031965ea0f127b6e98f6acc72720fe86a
hishamjaffar98 <hishamjaffar98@gmail.com>
2020-06-01 10:04:30 +0530

    Made Rocket Move

---
+++ Assets/Scripts/RocketShip.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketShip : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcesInput();
+    }
+
+    private void ProcesInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+           rigidBody.AddRelativeForce(Vector3.up);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+    }
+}
