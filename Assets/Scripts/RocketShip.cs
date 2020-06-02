9aef4ef2c24ca7e9805d5141e9a38ffc2d4c1dfd
hishamjaffar98 <hishamjaffar98@gmail.com>
2020-06-02 08:15:37 +0530

    Added Noise to Rocket

--- Assets/Scripts/RocketShip.cs
+++ Assets/Scripts/RocketShip.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class RocketShip : MonoBehaviour
 {
     Rigidbody rigidBody;
+    AudioSource audioSource;
+    //[SerializeField] AudioClip engineSound;
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -20,7 +23,15 @@ public class RocketShip : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-           rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up);
+            if(!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else if(Input.GetKeyUp(KeyCode.Space))
+        {
+            audioSource.Stop();
         }
 
         if (Input.GetKey(KeyCode.A))
